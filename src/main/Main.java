1e5145bee08393bf375403867349bf63c5b4a81c
Ioan-Cosmin Floroiu <floroiuioan@gmail.com>
2017-02-02 15:48:47 +0000

    icfloroiu branch

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //something something something truly inspiring
-        //changed sumtin sumtin etc
+        //added icfloroiu branch
         //TODO start working as a team
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
 
