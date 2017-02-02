45770cc96ab0b4475cc5694b09df05d14632071e
Ioan-Cosmin Floroiu <floroiuioan@gmail.com>
2017-02-02 15:45:57 +0000

    aleks dont be mad

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //something something something truly inspiring
+        //changed sumtin sumtin etc
         //TODO start working as a team
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
 
