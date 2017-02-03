8134792c0aec3332427c5365584e7fcca5d7b29a
astserba <astserba@gmail.com>
2017-02-03 16:21:00 +0000

    Proper project Structure

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //well
 
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml"));
 
