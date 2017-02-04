b89ca9cca75ae663e4ff57419aa273fcae669aa8
7DarkStar7 <posa_daniel7@yahoo.com>
2017-02-04 21:28:48 +0200

    blablalbbal

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -23,5 +23,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
         primaryStage.show();
+        //blalbalblalb
     }
 }
