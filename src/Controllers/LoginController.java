ee225b9c4ce761ba2655c576688c5e1e51ce0419
Cristian-Florin-Rotaru <crysss.floryn@gmail.com>
2017-02-04 19:47:51 +0000

    Login connected to the database so it can retrieve accounts

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,7 +1,9 @@
 package Controllers;
+import Modules.Login.LoginDB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -12,14 +14,20 @@ public class LoginController {
 
     @FXML private Text actionTarget;
     @FXML private TextField username;
-    @FXML private TextField pw;
+    @FXML private PasswordField pw;
     @FXML private Button editDetails;
 
     /**
-     * Example how it works.
-     * @param event
+     * Checks if the details entered correspond to an account
+     * that is located in database and says below the
+     * "Log in" button either true or false
      */
     @FXML protected void handleSubmitButtonAction (ActionEvent event) {
-        actionTarget.setText(username.getText() + " " + pw.getText());
+        actionTarget.setText(LoginDB.checkUserDetails(username.getText(), pw.getText()).toString());
+    }
+    @FXML Button loginButton;
+
+    @FXML protected void logIn (ActionEvent event) {
+        System.out.println(LoginDB.checkUserDetails(username.getText(), pw.getText()));
     }
 }
