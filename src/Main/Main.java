635c82caeaef7752d5f236a28e5ee737c2b042fe
KeylessBox <blueisbetteranytime@gmail.com>
2017-02-04 15:06:20 +0000

    minor changes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    // comment
     public static void main(String[] args) {
         launch(args);
     }
