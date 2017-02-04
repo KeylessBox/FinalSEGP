bb7813639d1baa4badd6b41b0fdafd68f13b84a7
Cristian-Florin-Rotaru <crysss.floryn@gmail.com>
2017-02-04 23:31:11 +0000

    Created "DeleteAccount" class inside the "ManageAccounts" package.

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -1,18 +1,23 @@
 package Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import Modules.Login.LoginDB;
-import Modules.Login.Login;
 import javafx.stage.Stage;
 
+
+import java.io.IOException;
 
 /**
  * Controller for login
  */
-public class LoginController {
+public class LoginController{
 
     @FXML private Text actionTarget;
     @FXML private TextField username;
@@ -23,8 +28,13 @@ public class LoginController {
      * Example how it works.
      * @param event
      */
-    @FXML protected void logIn (ActionEvent event) {
+    @FXML protected void logIn (ActionEvent event) throws IOException{
+
         if (!LoginDB.checkUserDetails(username.getText(), pw.getText())) {
+            /**
+             * If the account details introduced are wrong
+             * Alert Box is shown
+             */
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Wrong Details");
             alert.setHeaderText("Wrong Username or Password");
@@ -35,7 +45,17 @@ public class LoginController {
         }
         else
         {
-            
+            /**
+             * If the account details entered are correct,
+             * Program moves to the Main Window
+             */
+
+            Node node=(Node) event.getSource();
+            Stage stage=(Stage) node.getScene().getWindow();
+            Parent root2 = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml"));/* Exception */
+            Scene scene = new Scene(root2);
+            stage.setScene(scene);
+            stage.show();
         }
     }
 }
