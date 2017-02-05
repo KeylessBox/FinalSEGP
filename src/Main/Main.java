8f17ddf6f40c888b663b3023692a92e7d54a3d65
7DarkStar7 <posa_daniel7@yahoo.com>
2017-02-06 01:45:49 +0200

    Merge remote-tracking branch 'origin/dposa'
    # Conflicts:
    #	src/CSS/ACase1.css
    #	src/CSS/ACase2.css
    #	src/CSS/Add.css
    #	src/CSS/Background.css
    #	src/CSS/Background2.css
    #	src/CSS/Background3.css
    #	src/CSS/CaseBox.css
    #	src/CSS/CaseButton1.css
    #	src/CSS/CaseButton2.css
    #	src/CSS/CaseButton3.css
    #	src/CSS/CaseFile.css
    #	src/CSS/Cases.css
    #	src/CSS/Close.css
    #	src/CSS/Import.css
    #	src/CSS/Profile.css
    #	src/CSS/SaveAsBackground.css
    #	src/CSS/SaveAsButton.css
    #	src/CSS/Suspect.css
    #	src/CSS/Victim.css
    #	src/Controllers/MainController.java
    #	src/FXML/Main.fxml
    #	src/FXML/signIn.fxml
    #	src/Main/Main.java
    #	src/Modules/Login/Login.java
    #	src/SQL/load.sql

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    //dsdsd
     
     @Override
     public void start(Stage primaryStage) throws Exception {
