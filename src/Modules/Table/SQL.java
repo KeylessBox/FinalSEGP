372f130170d0a274dcc5b52ffd58f0146539ac3d
astserba <astserba@gmail.com>
2017-02-05 19:52:01 +0000

    Adding search bar

--- src/Modules/Table/SQL.java
+++ src/Modules/Table/SQL.java
@@ -81,4 +81,114 @@ public class SQL {
         }
         return true;
     }
+
+//    /**
+//     * Insert empty student record into database
+//     */
+//    public void addStudentQuery() {
+//
+//        Connection connection = dbConnection.connect();
+//
+//        try {
+//            connection.createStatement().executeUpdate("INSERT INTO `Students` (`studentID`, `studentName`, `studentUB`, `courseID`, `yearOfStudy`, `email`, `tutorID`) VALUES (" + (++maxIDStudent) + ", 'name surname', '00000000', '3', '4', 'example@example.com', '0')");
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//
+//    /**
+//     * Remove selected student record from database
+//     *
+//     * @param studentID
+//     */
+//    public void removeStudentQuery(String studentID) {
+//
+//        Connection connection = dbConnection.connect();
+//
+//        try {
+//            connection.createStatement().executeUpdate("DELETE FROM `Students` WHERE studentID = " + studentID);
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//
+//
+//    /**
+//     * Delete current table "Students" and fill it with new values
+//     *
+//     * @param studentList
+//     */
+//    public void saveStudentsQuery(ObservableList<StudentRecord> studentList) {
+//
+//        if (studentList != null) {
+//
+//            Connection connection = dbConnection.connect();
+//
+//            //delete all values from table:
+//            try {
+//                connection.createStatement().executeUpdate("DELETE FROM `Students`");
+//            } catch (SQLException ex) {
+//                ex.printStackTrace();
+//            }
+//
+//            maxIDStudent = 0;
+//
+//            System.out.println("\nSTUDENTS TABLE: \n");
+//
+//            for (StudentRecord studentRecord : studentList) {
+//
+//                try {
+//
+//                    String tutorID = studentRecord.getTutorID();
+//
+//                    if (!(isInteger(tutorID))) {
+//                        ResultSet temp;
+//                        temp = connection.createStatement().executeQuery("SELECT * FROM Tutors WHERE tutorName = '" + tutorID + "'");
+//                        if (temp.next()) {
+//                            tutorID = temp.getString(1);
+//                        }
+//
+//                        if (tutorID.equals("-")){
+//                            tutorID ="0";
+//                        }
+//                    }
+//
+//                    connection = dbConnection.connect();
+//                    String statement = "INSERT INTO `Students` (`studentID`, `studentName`, `studentUB`, `courseID`, `yearOfStudy`, `email`, `tutorID`) VALUES (" + (++maxIDStudent) + ", '" + studentRecord.getStudentName() + "', '" + studentRecord.getStudentUB() + "', '" + studentRecord.getCourseID() + "', '" + studentRecord.getYearOfStudy() + "', '" + studentRecord.getEmail() + "', '" + tutorID + "' )";
+//                    connection.createStatement().executeUpdate(statement);
+//
+//                    System.out.println("[ " + studentRecord.getStudentName() + " , " + studentRecord.getStudentUB() + " , " + studentRecord.getCourseID() + " , " + studentRecord.getYearOfStudy() + " , " + studentRecord.getEmail() + " , " + tutorID + " ]");
+//
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//
+//        }
+//    }
+//
+//    /**
+//     * Delete all tables values for new administrator
+//     */
+//    public void reset(){
+//
+//        Connection connection = dbConnection.connect();
+//
+//        //delete all values from table:
+//        try {
+//            connection.createStatement().executeUpdate("DELETE FROM `calls`");
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        connection = dbConnection.connect();
+//
+//        //delete all values from table:
+//        try {
+//            connection.createStatement().executeUpdate("DELETE FROM `calls`");
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+
 }
