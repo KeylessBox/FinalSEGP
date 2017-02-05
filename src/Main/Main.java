ef964f2cbd497d6b515c41ab96420dc6e519d5d0
astserba <astserba@gmail.com>
2017-02-05 22:00:04 +0000

    Tweaks

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -20,7 +20,6 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
     
     @Override
     public void start(Stage primaryStage) throws Exception {
