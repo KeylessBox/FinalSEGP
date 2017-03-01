a4d4e253b40cae5c0150587023f338db2334e682
Ioan-Cosmin Floroiu <floroiuioan@gmail.com>
2017-03-01 15:02:10 +0000

    test

---
+++ test/testytest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class testytest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
