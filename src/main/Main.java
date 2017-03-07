2e50e01b7ee4e29aa743884b44488216bf82dd9e
astserba <astserba@gmail.com>
2017-03-07 11:39:58 +0000

    style development

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,10 +31,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("Sign In");
+//        primaryStage.setResizable(false);
+//        primaryStage.setTitle("Sign In");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setMinHeight(924.0);
+        primaryStage.setMinWidth(1755.0);
         primaryStage.show();
     }
 }
